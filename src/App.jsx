3a9d741c732da1db5a786a68a073c27e17ffed82
Antonio Pontes <anvictorsantos@gmail.com>
2025-01-27 21:51:25 +0000

    chore: add Prettier and ESLint configuration files, update dependencies, and format code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 const App = () => {
   return (
     <div>
-  <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  </div>
-  )
-}
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
 
-export default App
+export default App;
