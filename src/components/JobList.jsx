281b6ac97aeb7b82b686d9a8df141939f270c31c
Antonio Pontes <anvictorsantos@gmail.com>
2025-02-06 17:12:40 +0000

    feat: add Spinner component for loading state in JobList and update data fetching logic

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from 'react';
+
 import PropTypes from 'prop-types';
 
-import jobs from '../jobs.json';
 import Job from './Job';
+import Spinner from './Spinner';
 
 const JobList = ({ isHome = false }) => {
-  const jobsList = isHome ? jobs.slice(0, 4) : jobs;
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      const apiUrl = isHome ? 'http://localhost:8000/jobs?_limit=4' : 'http://localhost:8000/jobs';
+      try {
+        const response = await fetch(apiUrl);
+        const jobs = await response.json();
+        setJobs(jobs);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchJobs();
+  }, []);
 
   return (
     <section className="bg-blue-50 px-4 pt-6 pb-10">
@@ -12,11 +32,16 @@ const JobList = ({ isHome = false }) => {
         <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
           {isHome ? 'Recent Jobs' : 'All Jobs'}
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          {jobsList.map((job) => (
-            <Job key={job.id} job={job} />
-          ))}
-        </div>
+
+        {loading ? (
+          <Spinner loading={loading} />
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            {jobs.map((job) => (
+              <Job key={job.id} job={job} />
+            ))}
+          </div>
+        )}
       </div>
     </section>
   );
---
+++ src/components/Spinner.jsx
@@ -0,0 +1,17 @@
+import { PropTypes } from 'prop-types';
+import { ClipLoader } from 'react-spinners';
+
+const override = {
+  display: 'block',
+  margin: '100px auto'
+};
+
+const Spinner = ({ loading }) => {
+  return <ClipLoader color="#4338CA" loading={loading} cssOverride={override} size={150} />;
+};
+
+Spinner.propTypes = {
+  loading: PropTypes.bool
+};
+
+export default Spinner;
